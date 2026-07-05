7bdb78fb1c87a4cfe1e144567ab9083216cae69a
monish-09 <mkha22647@gmail.com>
2026-07-05 21:40:55 +0530

    Configure Flask-Login and password hashing

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,19 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 from config import Config
-from app.extensions import db, migrate
+from app.extensions import db, migrate, login_manager
 from app.models import User
 from app.auth import auth
 
-
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(auth)
 
     app.config.from_object(Config)
 
+    app.register_blueprint(auth)
+
     db.init_app(app)
     migrate.init_app(app, db)
+    login_manager.init_app(app)
 
     @app.route("/")
     def home():
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,6 @@
 from .user import User
+from app.extensions import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
